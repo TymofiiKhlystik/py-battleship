e0eb4e715c823570cccd720f89c5bd0af753ac75
Tymofii Khlistik <tima0323199@gmail.com>
2025-02-10 21:24:54 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,21 +1,37 @@
 class Deck:
     def __init__(self, row, column, is_alive=True):
-        pass
+        self.row = row
+        self.column = column
+        self.is_alive = is_alive
 
 
 class Ship:
-    def __init__(self, start, end, is_drowned=False):
-        # Create decks and save them to a list `self.decks`
-        pass
+    def __init__(self, start: int, end: int) -> None:
+        self.decks = []
+        row1, col1 = start
+        row2, col2 = end
 
-    def get_deck(self, row, column):
-        # Find the corresponding deck in the list
-        pass
+        if row1 == row2:
+            for col in range(col1, col2 + 1):
+                self.decks.append(Deck(row1, col))
+        else:
+            for row in range(row1, row2 + 1):
+                self.decks.append(Deck(row, col1))
 
-    def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
-        pass
+    def get_deck(self, row: int, column: int) -> Deck:
+        for deck in self.decks:
+            if deck.row == row and deck.column == column:
+                return deck
+        return None
+
+    def fire(self, row: int, column: int) -> str:
+        deck = self.get_deck(row, column)
+        if deck and deck.is_alive:
+            deck.is_alive = False
+            if all(not d.is_alive for d in self.decks):
+                return "Sunk!"
+            return "Hit!"
+        return "Miss!"
 
 
 class Battleship:
